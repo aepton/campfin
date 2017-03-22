0ba21f1bea9f2d21200c2eb022d212c7fc31fa45
Abraham Epton <abraham.epton@socrata.com>
2017-03-21 23:34:17 -0700

    Adds missing import in settings

--- settings/settings.py
+++ settings/settings.py
@@ -1,3 +1,5 @@
+import os
+
 ROOT_DIRECTORY = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 DATA_DIRECTORY = os.path.join(ROOT_DIRECTORY, 'data')
 OCD_DIRECTORY = os.path.join(DATA_DIRECTORY, 'OCD')

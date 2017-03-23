e045923c1fd92a99c8d60cbe8117120eef1a8f6c
Abraham Epton <abraham.epton@socrata.com>
2017-03-22 19:11:02 -0700

    Restructuring DATA_URLS to fix bug with unordered fetching that has to be ordered

--- orchestration/generate_combined_ocd_data.py
+++ orchestration/generate_combined_ocd_data.py
@@ -13,15 +13,15 @@ YEARS = ['2018', '2016', '2014', '2012', '2010', '2008', '2006', '2004', '2002',
 DATA_URLS = {}
 for YEAR in YEARS:
   CYCLE = YEAR[2:]
-  DATA_URLS[YEAR] = {
-    'candidate_committee_link': 'ftp://ftp.fec.gov/FEC/%s/ccl%s.zip' % (YEAR, CYCLE),
-    'candidate_master': 'ftp://ftp.fec.gov/FEC/%s/cn%s.zip' % (YEAR, CYCLE),
-    'committee_master': 'ftp://ftp.fec.gov/FEC/%s/cm%s.zip' % (YEAR, CYCLE),
-    'committee_to_committee': 'ftp://ftp.fec.gov/FEC/%s/oth%s.zip' % (YEAR, CYCLE),
-    'committee_to_candidate': 'ftp://ftp.fec.gov/FEC/%s/pas2%s.zip' % (YEAR, CYCLE),
-    'expenditures': 'ftp://ftp.fec.gov/FEC/%s/oppexp%s.zip' % (YEAR, CYCLE),
-    'contributions': 'ftp://ftp.fec.gov/FEC/%s/indiv%s.zip' % (YEAR, CYCLE)
-  }
+  DATA_URLS[YEAR] = [
+    ('candidate_committee_link', 'ftp://ftp.fec.gov/FEC/%s/ccl%s.zip' % (YEAR, CYCLE)),
+    ('candidate_master', 'ftp://ftp.fec.gov/FEC/%s/cn%s.zip' % (YEAR, CYCLE)),
+    ('committee_master', 'ftp://ftp.fec.gov/FEC/%s/cm%s.zip' % (YEAR, CYCLE)),
+    ('committee_to_committee', 'ftp://ftp.fec.gov/FEC/%s/oth%s.zip' % (YEAR, CYCLE)),
+    ('committee_to_candidate', 'ftp://ftp.fec.gov/FEC/%s/pas2%s.zip' % (YEAR, CYCLE)),
+    ('expenditures', 'ftp://ftp.fec.gov/FEC/%s/oppexp%s.zip' % (YEAR, CYCLE)),
+    ('contributions', 'ftp://ftp.fec.gov/FEC/%s/indiv%s.zip' % (YEAR, CYCLE))
+  ]
 
 def cleanup_data_dirs():
   for state in settings.STATES_IMPLEMENTED:
@@ -35,7 +35,7 @@ def download_and_process_fec_data():
 
   for year in YEARS:
     print 'Year %s' % year
-    for url_type, url in DATA_URLS[year].items():
+    for url_type, url in DATA_URLS[year]:
       file_path = fec_fetch.download_data(url, url_type, year)
       fec_transform.transform_data(file_path, url_type)
       fec_fetch.cleanup_data(url_type, year)

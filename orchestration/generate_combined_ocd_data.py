78460c83f1334916f6bace2401b6c537a59caf91
Abraham Epton <abraham.epton@socrata.com>
2017-03-29 11:37:48 -0700

    Fixes typo

--- orchestration/generate_combined_ocd_data.py
+++ orchestration/generate_combined_ocd_data.py
@@ -56,7 +56,7 @@ def upload_to_socrata():
   dataset = 'fac5-i55pj'
   print subprocess.Popen(
     "java -jar %s -c %s -f %s -h true -m upsert -ph true -cf %s -i %s" % (
-      datasync, config, csv, control, dataset)
+      datasync, config, csv, control, dataset),
     shell=True,
     stdout=subprocess.PIPE).stdout.read()
 
